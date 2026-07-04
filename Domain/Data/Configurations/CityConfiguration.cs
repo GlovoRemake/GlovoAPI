dc54f7977ce0495bbde1e4be67a87248a734ca55
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-05 00:11:59 +0300

    transfering db configutaions to multiply files

---
+++ Domain/Data/Configurations/CityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Data.Configurations;
+
+public class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder
+            .HasOne(c => c.Region)
+            .WithMany(r => r.Cities)
+            .HasForeignKey(c => c.RegionId);
+    }
+}
---
+++ Domain/Data/Configurations/UserOrderConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Domain.Entities.Order;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Domain.Data.Configurations;
+
+public class UserOrderConfiguration : IEntityTypeConfiguration<UserOrder>
+{
+    public void Configure(EntityTypeBuilder<UserOrder> builder)
+    {
+        builder
+            .HasOne(c => c.User)
+            .WithMany(c => c.UserOrders)
+            .HasForeignKey(c => c.UserId);
+        builder
+            .HasOne(c => c.UserLocation)
+            .WithMany(c => c.UserOrders)
+            .HasForeignKey(c => c.LocationId);
+        builder
+            .HasOne(c => c.Promocode)
+            .WithMany(c => c.UserOrders)
+            .HasForeignKey(c => c.PromocodeId);
+    }
+}
