13c0eb14b5d5ddb91622950d9456c6dfd97bb82d
CODER <CODER@A24PC8>
2026-07-05 14:26:17 +0300

    add base entitites

---
+++ Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Domain.Entities.Base;
+
+public class BaseEntity<T> : IEntity<T>
+{
+    public T Id { get; set; }
+    public DateTime DateCreated { get; set; } = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+    public DateTime DateUpdated { get; set; } = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+}
