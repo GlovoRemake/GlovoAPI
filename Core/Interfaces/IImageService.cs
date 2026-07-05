998f69713899f7f02f9feb4399386880d2e7d32c
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-07-05 21:30:47 +0300

    addImageService для тесту ImageService було додано TestController, можна видалити, якщо потрібно буде

---
+++ Core/Interfaces/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces;
+
+public interface IImageService
+{
+    Task<string> SaveImageAsync(IFormFile file);
+    Task<string> SaveImageFromUrlAsync(string imageUrl);
+    Task<string> SaveImageFromBase64Async(string input);
+    Task DeleteImageAsync(string name);
+}
