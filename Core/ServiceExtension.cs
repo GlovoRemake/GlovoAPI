7de2d09307e602d752279f85800c60940648ca2e
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-07-05 21:30:47 +0300

    addImageService для тесту ImageService було додано TestController, можна видалити, якщо потрібно буде

--- Core/ServiceExtension.cs
+++ Core/ServiceExtension.cs
@@ -1,14 +1,19 @@
 ﻿using Core.Interfaces;
 using Core.Repositories;
+using Core.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Core;
 
-public static class ServiceExstension
+public static class ServiceExtension
 {
     public static void AddRepositories(this IServiceCollection service)
     {
         service.AddScoped(typeof(IRepository<,>), typeof(Repository<,>));
         service.AddScoped(typeof(ISoftDeleteRepository<,>), typeof(SoftDeleteRepository<,>));
+    }
+    public static void AddImageService(this IServiceCollection service)
+    {
+        service.AddScoped<IImageService, ImageService>();
     }
 }
