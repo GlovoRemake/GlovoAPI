77f1abad8e34286a1dc21fbee172be4ff4784827
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-08 00:04:43 +0300

    Add refresh token table

---
+++ Domain/Entities/User/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Identity;
+using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Domain.Entities.User;
+
+public class RefreshToken : BaseEntity<int>
+{
+    public string Token { get; set; } = null!;
+    public Guid UserId { get; set; }
+
+    public DateTime Expires { get; set; }
+    public bool IsRevoked { get; set; }
+
+    // conn
+    public UserEntity User { get; set; }
+}
