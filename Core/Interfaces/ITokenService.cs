7f4fea2d609ec015091f3649f67b0f06f93d468f
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-08 00:25:50 +0300

    Fully code token service

--- Core/Interfaces/ITokenService.cs
+++ Core/Interfaces/ITokenService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities.Identity;
+using Domain.Entities.User;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,4 +8,9 @@ namespace Core.Interfaces;
 
 public interface ITokenService
 {
+    Task<string> CreateTokenAsync(UserEntity user);
+    Task<string> CreateRegistrationTokenAsync(string email);
+    Task<RefreshToken> GenerateRefreshTokenAsync(UserEntity user);
+    Task<RefreshToken?> ValidateRefreshTokenAsync(string token);
+    Task RevokeRefreshTokenAsync(string token);
 }
