8679e7a522d023112f00a3ad6cec5b9b915a65ab
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-12 17:59:48 +0300

    add HashService

---
+++ Core/Services/HashService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Core.Interfaces;
+
+namespace Core.Services;
+
+public class HashService : IHashService
+{
+    public string Hash(string input)
+    {
+        using var sha = SHA256.Create();
+        var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+        return Convert.ToBase64String(bytes);
+    }
+}
