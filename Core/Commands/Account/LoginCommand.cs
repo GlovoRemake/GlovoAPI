6bd450c3b76dbb827d42ebec55aafbc2d8338342
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-13 00:48:14 +0300

    add commands

---
+++ Core/Commands/Account/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using Core.Dtos.Account;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Commands.Account;
+
+public record LoginCommand(string email, string password)
+    : IRequest<TokenResponseDto>;
---
+++ Core/Commands/Account/VerifyCodeCommand.cs
@@ -0,0 +1,10 @@
+﻿using Core.Dtos.Account;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Commands.Account;
+
+public record VerifyCodeCommand(VerifyCodeDto dto)
+    : IRequest<TokenResponseDto>;
