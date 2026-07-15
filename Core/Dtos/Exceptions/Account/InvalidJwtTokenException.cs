f523cf8b7e498a875b9c3c871a41e8573969c3fd
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-07-16 02:03:07 +0300

    create GetProfile(), made 2 exceptions and AccountMapper

---
+++ Core/Dtos/Exceptions/Account/InvalidJwtTokenException.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Dtos.Exceptions.Account;
+
+public class InvalidJwtTokenException : Exception
+{
+	public InvalidJwtTokenException()
+        : base("") { }
+
+    public InvalidJwtTokenException(string message)
+        : base(message) { }
+}
---
+++ Core/Mappers/AccountMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Core.Dtos.Account;
+using Core.Entities.Identity;
+
+namespace Core.Mappers;
+
+public class AccountMapper : Profile
+{
+    public AccountMapper()
+    {
+        CreateMap<UserEntity, GetProfileDto>();
+    }
+}
