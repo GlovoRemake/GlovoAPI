ef782e33636c7320f3fe28e790a6bd0f4197faa5
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-19 18:11:01 +0300

    integtate to MediatR and Controller

---
+++ Core/Commands/Partner/PartnerLoginCommand.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+using Core.Dtos.Account;
+using MediatR;
+
+namespace Core.Commands.Partner;
+
+public record PartnerLoginCommand(string email, string password)
+    : IRequest<Result<TokenResponseDto>>;
---
+++ Core/Commands/Partner/PartnerRegisterCommand.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+using Core.Dtos.Partner;
+using MediatR;
+
+namespace Core.Commands.Partner;
+
+public record PartnerRegisterCommand(PartnerRegisterDto dto)
+    : IRequest<Result>;
