b22a583ebfd22ea96d8dbb170465747083c939f8
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-19 18:11:01 +0300

    integtate to MediatR and Controller

---
+++ Core/Handlers/Partner/PartnerRefreshTokenCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using Core.Commands.Partner;
+using Core.Dtos;
+using Core.Dtos.Account;
+using Core.Dtos.Exceptions.Account;
+using Core.Interfaces;
+using MediatR;
+
+namespace Core.Handlers.Partner;
+
+
+public sealed class PartnerRefreshTokenCommandHandler
+    : IRequestHandler<PartnerRefreshTokenCommand, Result<TokenResponseDto>>
+{
+    private readonly IPartnerService _partnerService;
+
+    public PartnerRefreshTokenCommandHandler(IPartnerService partnerService)
+    {
+        _partnerService = partnerService;
+    }
+
+    public async Task<Result<TokenResponseDto>> Handle(
+        PartnerRefreshTokenCommand request,
+        CancellationToken cancellationToken)
+    {
+        TokenResponseDto res;
+
+        try
+        {
+            res = await _partnerService.RefreshToken(request.refreshToken);
+
+        }
+        catch (InvalidRefreshTokenException)
+        {
+            return Result<TokenResponseDto>.Failure(ErrorMessage.Create(
+                "Token",
+                $"Невірний токен оновлення"
+            ));
+        }
+        catch (Exception ex)
+        {
+            return Result<TokenResponseDto>.Failure(ErrorMessage.Create(
+                "ServerError",
+                $"An error occurred during token refresh: {ex.Message}"
+            ));
+        }
+
+        return Result<TokenResponseDto>.Success(res);
+    }
+}
