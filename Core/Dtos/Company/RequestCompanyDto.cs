a792ac75eb85e6c5f900e798d66e352b2ac5a52e
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-26 16:01:12 +0300

    add requests

--- Core/Dtos/Company/RequestCompanyDto.cs
+++ Core/Dtos/Company/RequestCompanyDto.cs
@@ -6,8 +6,11 @@ namespace Core.Dtos.Company;
 
 public class RequestCompanyDto
 {
+    public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public bool? IsApprove { get; set; }
+    public string? Message { get; set; }
     public Guid PartnerId { get; set; }
     public Guid? CompanyId { get; set; }
 }
