fddb1a08bbccb5751b0351a48737344caae1f9e8
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-26 15:56:16 +0300

    add approval

---
+++ Core/Dtos/Company/ApprovalCompanyDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company;
+
+public class ApprovalCompanyDto
+{
+    public long RequestId { get; set; }
+    public bool IsApprove { get; set; }
+}
