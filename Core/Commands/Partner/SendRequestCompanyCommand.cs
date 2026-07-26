68981fa6ae7a6e417e83fa0c7949f77885dcacdd
BloodyRoom <sabadashkamaksym@gmail.com>
2026-07-26 16:38:58 +0300

    add MediatR integration and controllers

---
+++ Core/Commands/Partner/SendRequestCompanyCommand.cs
@@ -0,0 +1,11 @@
+﻿using Core.Dtos;
+using Core.Dtos.Company;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Commands.Partner;
+
+public record SendRequestCompanyCommand(string email, AddRequestCompanyDto dto)
+    : IRequest<Result>;
---
+++ Core/Handlers/Company/ApprovalRequestCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using Core.Commands.Company;
+using Core.Commands.Partner;
+using Core.Dtos;
+using Core.Dtos.Exceptions.Account;
+using Core.Dtos.Exceptions.Company;
+using Core.Interfaces;
+using Core.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Handlers.Company;
+
+public sealed class ApprovalRequestCommandHandler
+    : IRequestHandler<ApprovalRequestCommand, Result>
+{
+    private readonly ICompanyService _companyService;
+
+    public ApprovalRequestCommandHandler(ICompanyService companyService)
+    {
+        _companyService = companyService;
+    }
+
+    public async Task<Result> Handle(
+        ApprovalRequestCommand request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _companyService.ApprovalRequest(request.dto);
+        }
+        catch (RequestNotFoundException)
+        {
+            return Result.Failure(ErrorMessage.Create(
+                "RequestNotFound",
+                $"Запит не знайдено"
+            ));
+        }
+        catch (Exception ex)
+        {
+            return Result.Failure(ErrorMessage.Create(
+                "ServerError",
+                $"An error occurred during registration: {ex.Message}"
+            ));
+        }
+
+        return Result.Success();
+    }
+}
