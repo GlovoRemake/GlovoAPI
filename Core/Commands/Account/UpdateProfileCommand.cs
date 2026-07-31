4a026f8f8c70cd8cb4c29f438ef73225903cd149
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-07-31 14:09:29 +0300

    Create UpdateProfileCommand & Handler in MediatR

---
+++ Core/Commands/Account/UpdateProfileCommand.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+using Core.Dtos.Account;
+using MediatR;
+
+namespace Core.Commands.Account;
+
+public record UpdateProfileCommand(string email, UpdateProfileDto dto)
+    : IRequest<Result>;
