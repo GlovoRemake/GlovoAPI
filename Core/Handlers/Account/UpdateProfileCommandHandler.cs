b05530a079cf535fa9d6ab62bbde909afad405c6
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-07-31 14:09:29 +0300

    Create UpdateProfileCommand & Handler in MediatR

---
+++ Core/Handlers/Account/UpdateProfileCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Core.Commands.Account;
+using Core.Dtos;
+using Core.Dtos.Exceptions.Account;
+using Core.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Handlers.Account;
+
+public sealed class UpdateProfileCommandHandler : IRequestHandler<UpdateProfileCommand, Result>
+{
+    private readonly IAccountService _accountService;
+
+    public UpdateProfileCommandHandler(IAccountService accountService, IConfiguration config)
+    {
+        _accountService = accountService;
+    }
+    public async Task<Result> Handle(UpdateProfileCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _accountService.UpdateProfileAsync(request.email, request.dto);
+        }
+        catch (AnotherTypeRegException ex)
+        {
+            return Result.Failure(ErrorMessage.Create(
+                "AnotherTypeReg",
+                $"Користувач зареєстрований іншим способом ({ex.Message})"
+            ));
+        }
+        catch (UserNotFoundException ex)
+        {
+            return Result.Failure(ErrorMessage.Create(
+                "User",
+                $"{ex.Message}"
+            ));
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        return Result.Success();
+    }
+}
