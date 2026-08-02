afdf7f4d88f8c75e3abaca1128acef52700922e1
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-02 15:12:01 +0300

    add DTOs

---
+++ Core/Dtos/Company/Category/GetAllCategoriesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Dtos.Company.Category;
+
+public class GetAllCategoriesDto
+{
+    public Guid CompanyId { get; set; }
+    // пагінація
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
