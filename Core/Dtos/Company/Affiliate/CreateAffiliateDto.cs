92753ee167eb5ab0f22cd9a3c295a72e911e7916
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-02 15:58:38 +0300

    add bare service and dtos

---
+++ Core/Dtos/Company/Affiliate/CreateAffiliateDto.cs
@@ -0,0 +1,13 @@
+﻿
+using Core.Dtos.Company.Affiliate.Location;
+
+namespace Core.Dtos.Company.Affiliate;
+
+public class CreateAffiliateDto
+{
+    public Guid CompanyId { get; set; }
+    public string Phone { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public CreateAffiliateLocationDto Location { get; set; } = default!;
+}
---
+++ Core/Dtos/Company/Affiliate/Location/CreateAffiliateLocationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Affiliate.Location;
+
+public class CreateAffiliateLocationDto
+{
+    public string Location { get; set; } = string.Empty;
+    public int RegionId { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public string PostalIndex { get; set; } = string.Empty;
+}
