af0082c07df1199bd3bfea5ff0558c741bf829e3
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-02 15:12:39 +0300

    add CompanyCategoryService

---
+++ Core/Services/CompanyCategoryService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoMapper.Internal.Mappers;
+using Core.Dtos.Company;
+using Core.Dtos.Company.Category;
+using Core.Dtos.Exceptions.Company;
+using Core.Interfaces;
+using Domain.Entities.Company;
+using Domain.Entities.Company.ProductCategory;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services;
+
+public class CompanyCategoryService(
+        IMapper _mapper
+    ) : ICompanyCategoryService
+{
+    public Task<List<CategoryDto>> GetAllCompanyCategoriesAsync(GetAllCategoriesDto query)
+    {
+        throw new NotImplementedException();
+    }
+}
