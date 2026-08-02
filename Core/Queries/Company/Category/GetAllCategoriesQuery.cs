04c02f7e2e2e0984962550dc3a1510c043589db6
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-02 15:13:06 +0300

    Add Handler

---
+++ Core/Queries/Company/Category/GetAllCategoriesQuery.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+using Core.Dtos.Company.Category;
+using MediatR;
+
+namespace Core.Queries.Company.Category;
+
+public record GetAllCategoriesQuery(GetAllCategoriesDto dto)
+    : IRequest<Result<List<CategoryDto>>>;
