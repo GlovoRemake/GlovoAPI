a93f83c2be393d6f6eb95cedc99d68bdc526b463
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-02 15:13:06 +0300

    Add Handler

---
+++ Core/Handlers/Company/Category/GetAllCategoriesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Core.Dtos;
+using Core.Dtos.Company.Category;
+using Core.Interfaces;
+using Core.Queries.Company.Category;
+using MediatR;
+
+namespace Core.Handlers.Company.Category;
+
+public sealed class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, Result<List<CategoryDto>>>
+{
+    private readonly ICompanyCategoryService _companyCategoryService;
+
+    public GetAllCategoriesQueryHandler(ICompanyCategoryService companyCategoryService)
+    {
+        _companyCategoryService = companyCategoryService;
+    }
+    public async Task<Result<List<CategoryDto>>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var res = await _companyCategoryService.GetAllCompanyCategoriesAsync(request.dto);
+        return Result<List<CategoryDto>>.Success(res);
+    }
+}
