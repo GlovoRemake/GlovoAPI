cb78c8e9b13cba0ea006da78f17e8b972a7d5c2f
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-02 18:34:40 +0300

    add courier table & some fixes

---
+++ Domain/Entities/Courier/CourierProfile.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Identity;
+using Domain.Entities.Base;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities.Courier;
+
+public class CourierProfile : BaseEntityWithIsDeleted<int>
+{
+    public Guid UserId { get; set; }
+    public double Balance { get; set; }
+    public TransportType TransportType { get; set; }
+
+    // conn
+    public UserEntity User { get; set; } = default!;
+}
