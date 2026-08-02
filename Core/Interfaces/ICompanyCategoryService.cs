005088fdaea6bef8bb5404232fcb45690b6c4168
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-02 15:12:39 +0300

    add CompanyCategoryService

---
+++ Core/Interfaces/ICompanyCategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Dtos.Company;
+using Core.Dtos.Company.Category;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces;
+
+public interface ICompanyCategoryService
+{
+    Task<List<CategoryDto>> GetAllCompanyCategoriesAsync(GetAllCategoriesDto query);
+}
