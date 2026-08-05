77df3b6a0ebdb906ddbe45aca81abc7e9b6d77a5
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-05 13:46:30 +0300

    integrate services mediatr and controller

---
+++ Core/Handlers/Company/Product/DeleteProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Core.Commands.Company.Product;
+using Core.Dtos;
+using Core.Dtos.Company.Product;
+using Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Handlers.Company.Product;
+
+public sealed class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Result<bool>>
+{
+    private readonly ICompanyProductService _companyProductService;
+
+    public DeleteProductCommandHandler(ICompanyProductService companyProductService)
+    {
+        _companyProductService = companyProductService;
+    }
+    public async Task<Result<bool>> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await _companyProductService.DeleteProductAsync(request.ProductId);
+            return Result<bool>.Success(result);
+        }
+        catch (Exception ex)
+        {
+            return Result<bool>.Failure(new ErrorMessage("DeleteProductError", ex.Message));
+        }
+    }
+}
---
+++ Core/Handlers/Company/Product/UpdateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Core.Commands.Company.Product;
+using Core.Dtos;
+using Core.Dtos.Company.Product;
+using Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Handlers.Company.Product;
+
+public sealed class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, Result<ProductDto>>
+{
+    private readonly ICompanyProductService _companyProductService;
+
+    public UpdateProductCommandHandler(ICompanyProductService companyProductService)
+    {
+        _companyProductService = companyProductService;
+    }
+    public async Task<Result<ProductDto>> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var product = await _companyProductService.UpdateProductAsync(request.ProductId, request.Dto);
+            return Result<ProductDto>.Success(product);
+        }
+        catch (Exception ex)
+        {
+            return Result<ProductDto>.Failure(new ErrorMessage("UpdateProductError", ex.Message));
+        }
+    }
+}
