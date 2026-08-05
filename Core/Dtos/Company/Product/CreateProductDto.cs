b2ef76ce523e3e587776c218d8707df4768f30be
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-05 13:13:37 +0300

    add dtos

---
+++ Core/Dtos/Company/Product/CreateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Product;
+
+public class CreateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public IFormFile Image { get; set; } = default!;
+    public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public double Weight { get; set; }
+    public double Kcal { get; set; }
+}
---
+++ Core/Dtos/Company/Product/UpdateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Product;
+
+public class UpdateProductDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public IFormFile? Image { get; set; }
+    public double Price { get; set; }
+    public int CategoryId { get; set; }
+    public double Weight { get; set; }
+    public double Kcal { get; set; }
+}
