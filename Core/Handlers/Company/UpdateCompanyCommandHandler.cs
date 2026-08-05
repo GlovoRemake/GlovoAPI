5f37f45e783cbc7eccaca5e332bf5a5bb2c44ec5
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-08-05 14:32:22 +0300

    update company

---
+++ Core/Handlers/Company/UpdateCompanyCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Core.Commands.Company;
+using Core.Dtos;
+using Core.Dtos.Exceptions.Company;
+using Core.Interfaces;
+using MediatR;
+
+namespace Core.Handlers.Company;
+
+public sealed class UpdateCompanyCommandHandler : IRequestHandler<UpdateCompanyCommand, Result>
+{
+    private readonly ICompanyService _companyService;
+
+    public UpdateCompanyCommandHandler(ICompanyService companyService)
+    {
+        _companyService = companyService;
+    }
+    public async Task<Result> Handle(UpdateCompanyCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _companyService.UpdateCompanyAsync(request.dto);
+            return Result.Success();
+        }
+        catch (CompanyNotFoundException ex)
+        {
+            return Result.Failure(new ErrorMessage("CompanyNotFound", ex.Message));
+        }
+        catch (Exception)
+        {
+            return Result.Failure(new ErrorMessage("UpdateCompanyError", "Виникла помилка при оновленні компанії."));
+        }
+    }
+}
