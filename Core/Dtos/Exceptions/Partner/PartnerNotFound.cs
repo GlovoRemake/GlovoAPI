41e9fce709d0dd03e86b668703d44e9637373a08
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-11 16:20:58 +0300

    Add dtos

---
+++ Core/Dtos/Exceptions/Partner/PartnerNotFound.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Exceptions.Partner;
+
+public class PartnerNotFound : Exception
+{
+    public PartnerNotFound()
+        : base("") { }
+    public PartnerNotFound(string message)
+        : base(message) { }
+}
