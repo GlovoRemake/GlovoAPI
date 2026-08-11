79b17c152e88abc227f21a4cffc5fc29a5f50bde
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-11 16:21:13 +0300

    add manager logic

--- Core/Services/AffiliateService.cs
+++ Core/Services/AffiliateService.cs
@@ -4,14 +4,17 @@ using Core.Dtos.Company;
 using Core.Dtos.Company.Affiliate;
 using Core.Dtos.Exceptions.Company;
 using Core.Dtos.Exceptions.Company.Affiliate;
+using Core.Dtos.Exceptions.Partner;
 using Core.Dtos.Exceptions.Region;
 using Core.Interfaces;
 using Domain.Entities;
 using Domain.Entities.Company;
 using Domain.Entities.Company.Affiliate;
+using Domain.Entities.Company.Partner;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Core.Services;
@@ -20,7 +23,10 @@ public class AffiliateService(
         ISoftDeleteRepository<Company, Guid> _companyRepo,
         IRepository<CompanyAffiliate, Guid> _affiliateRepo,
         IRepository<CompanyAffiliateLocation, int> _locationRepo,
+        ISoftDeleteRepository<PartnerUser, Guid> _partnerRepo,
         ISoftDeleteRepository<Region, int> _regionRepo,
+        ISoftDeleteRepository<Employee, int> _employeeRepo,
+        ISoftDeleteRepository<PartnerRole, int> _partnerRoleRepo,
         IMapper _mapper
     ) : IAffiliateService
 {
@@ -92,5 +98,47 @@ public class AffiliateService(
         await _affiliateRepo.DeleteAsync(affiliate.Id);
         await _locationRepo.DeleteAsync(affiliate.LocationId);
         return true;
+    }
+
+    public async Task AddManager(Guid affiliateId, OperationAffiliateUserDto partnerDto)
+    {
+        var affiliate = await _affiliateRepo.Query().AnyAsync(x => x.Id == affiliateId);
+        if (!affiliate)
+            throw new AffiliateNotFoundException();
+
+        var partner = await _partnerRepo.Query().FirstOrDefaultAsync(x => x.Email == partnerDto.PartnerEmail && !x.IsDeleted);
+        if (partner == null)
+            throw new PartnerNotFound();
+
+        var isAlready = await _employeeRepo.Query().AnyAsync(x => x.CompanyAffiliateId == affiliateId && x.PartnerUserId == partner.Id && !x.IsDeleted);
+        if (isAlready)
+            throw new PartnerEmailAlreadyRegistered();
+
+        await _employeeRepo.AddAsync(new Employee
+        {
+            PartnerUserId = partner.Id,
+            CompanyAffiliateId = affiliateId,
+            RoleId = await _partnerRoleRepo.Query().Where(x => x.Name == "Manager").Select(x => x.Id).FirstOrDefaultAsync()
+        });
+    }
+
+    public async Task RemoveManager(Guid affiliateId, OperationAffiliateUserDto partnerDto)
+    {
+        var affiliate = await _affiliateRepo.Query().AnyAsync(x => x.Id == affiliateId);
+        if (!affiliate)
+            throw new AffiliateNotFoundException();
+
+        var partner = await _partnerRepo.Query().FirstOrDefaultAsync(x => x.Email == partnerDto.PartnerEmail && !x.IsDeleted);
+        if (partner == null)
+            throw new PartnerNotFound();
+
+        var employee = await _employeeRepo.Query()
+            .Where(x => x.CompanyAffiliateId == affiliateId && x.PartnerUserId == partner.Id && !x.IsDeleted)
+            .Select(x => (int?)x.Id)
+            .FirstOrDefaultAsync();
+        if (employee == null)
+            throw new PartnerNotFound();
+
+        await _employeeRepo.DeleteAsync(employee ?? -1);
     }
 }
