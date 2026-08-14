60384238c084870928b286d01feba124bae87f20
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-14 17:31:56 +0300

    Create dtos

---
+++ Core/Dtos/Company/Product/Additional/AdditionalDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Product.Additional;
+
+public class AdditionalDto
+{
+    public int Id { get; set; }
+    public double Price { get; set; }
+    public int Order { get; set; }
+}
---
+++ Core/Dtos/Company/Product/AdditionalGroup/AdditionalGroupDto.cs
@@ -0,0 +1,17 @@
+﻿using Core.Dtos.Company.Product.Additional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Product.AdditionalGroup;
+
+public class AdditionalGroupDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int MinChoice { get; set; }
+    public int MaxChoice { get; set; }
+    public int Order { get; set; }
+    public int ProductId { get; set; }
+    public List<AdditionalDto> Additionals { get; set; } = default!;
+}
