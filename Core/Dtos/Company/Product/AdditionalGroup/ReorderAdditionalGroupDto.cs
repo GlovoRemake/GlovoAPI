c807b2a440e470c29f6c1c81157274cc9e502c20
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-14 17:41:08 +0300

    implement service withoult logic

---
+++ Core/Dtos/Company/Product/AdditionalGroup/ReorderAdditionalGroupDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Dtos.Company.Product.AdditionalGroup;
+
+public class ReorderAdditionalGroupDto
+{
+    public List<int> Ids { get; set; } = default!;
+}
---
+++ Core/Services/CompanyProductAdditionalService.cs
@@ -0,0 +1,35 @@
+﻿using Core.Dtos.Company.Product.AdditionalGroup;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services;
+
+public class CompanyProductAdditionalService : ICompanyProductAdditionalService
+{
+    public Task<List<AdditionalGroupDto>> GetAdditionalsGroup(int additionalGroupId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AdditionalGroupDto> UpdateAdditionalGroup(int additionalGroupId, UpdateAdditionalGroupDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AdditionalGroupDto> CreateAdditionalGroup(int productId, CreateAdditionalGroupDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAdditionalGroup(int additionalGroupId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AdditionalGroupDto> ReorderAdditionalGroup(int additionalGroupId, ReorderAdditionalGroupDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
