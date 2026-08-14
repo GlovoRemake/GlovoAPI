ca58937e48bc8cc0d2c68a02d4628ab7e7f5d9b0
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-14 21:27:44 +0300

    Add create additional

---
+++ Core/Mappers/AdditionalMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core.Dtos.Company.Product.Additional;
+using Domain.Entities.Company.Product.Additional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Mappers;
+
+public class AdditionalMapper : Profile
+{
+    public AdditionalMapper()
+    {
+        CreateMap<CreateAdditionalDto, Additional>();
+        CreateMap<Additional, AdditionalDto>();
+    }
+}
