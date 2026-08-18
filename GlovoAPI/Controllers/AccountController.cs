f9c855b22abf4e29257e9306106ca8dc261401e7
BloodyRoom <sabadashkamaksym@gmail.com>
2026-08-19 01:56:02 +0300

    Add httpOnly cookies to account controller

--- GlovoAPI/Controllers/AccountController.cs
+++ GlovoAPI/Controllers/AccountController.cs
@@ -1,17 +1,18 @@
 ﻿using Core.Commands.Account;
+using Core.Commands.Partner;
 using Core.Dtos.Account;
+using Core.Queries.Account;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
-using Core.Queries.Account;
 
 namespace GlovoAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController(IMediator _mediator) : ControllerBase
+    public class AccountController(IMediator _mediator, IConfiguration _config) : ControllerBase
     {
         [Authorize(AuthenticationSchemes = "RegistrationScheme")]
         [HttpPost("Register")]
@@ -24,6 +25,21 @@ namespace GlovoAPI.Controllers
 
             if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
 
+
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
+
+
             return Ok(new { result.IsSuccess, result.Value });
         }
 
@@ -34,6 +50,31 @@ namespace GlovoAPI.Controllers
 
             if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
 
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+            var lifeTimeRefresh = _config["Tokens:Refresh:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
+
+            Response.Cookies.Append(
+               "refreshToken",
+               result.Value.RefreshToken,
+               new CookieOptions
+               {
+                   HttpOnly = true,
+                   Secure = true,
+                   SameSite = SameSiteMode.None,
+                   Expires = DateTime.UtcNow.AddDays(int.TryParse(lifeTimeRefresh, out var days) ? days : 7),
+               });
+
             return Ok(new { result.IsSuccess, result.Value });
         }
 
@@ -43,6 +84,31 @@ namespace GlovoAPI.Controllers
             var result = await _mediator.Send(new GoogleLoginCommand(request));
 
             if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
+
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+            var lifeTimeRefresh = _config["Tokens:Refresh:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
+
+            Response.Cookies.Append(
+               "refreshToken",
+               result.Value.RefreshToken,
+               new CookieOptions
+               {
+                   HttpOnly = true,
+                   Secure = true,
+                   SameSite = SameSiteMode.None,
+                   Expires = DateTime.UtcNow.AddDays(int.TryParse(lifeTimeRefresh, out var days) ? days : 7),
+               });
 
             return Ok(new { result.IsSuccess, result.Value });
         }
@@ -64,15 +130,82 @@ namespace GlovoAPI.Controllers
 
             if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
 
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+            var lifeTimeRefresh = _config["Tokens:Refresh:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
+
+            Response.Cookies.Append(
+               "refreshToken",
+               result.Value.RefreshToken,
+               new CookieOptions
+               {
+                   HttpOnly = true,
+                   Secure = true,
+                   SameSite = SameSiteMode.None,
+                   Expires = DateTime.UtcNow.AddDays(int.TryParse(lifeTimeRefresh, out var days) ? days : 7),
+               });
+
             return Ok(new { result.IsSuccess, result.Value });
         }
 
         [HttpPost("Refresh")]
-        public async Task<IActionResult> Refresh([FromBody] RefreshRequest refreshRequest)
+        public async Task<IActionResult> Refresh()
         {
-            var result = await _mediator.Send(new RefreshTokenCommand(refreshRequest.RefreshToken));
+            if (!Request.Cookies.TryGetValue(
+                    "refreshToken",
+                    out var refreshToken))
+            {
+                return Unauthorized();
+            }
 
-            if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
+            var result = await _mediator.Send(new PartnerRefreshTokenCommand(refreshToken));
+
+            if (!result.IsSuccess)
+            {
+                Response.Cookies.Delete("accessToken");
+                Response.Cookies.Delete("refreshToken");
+
+                return Unauthorized(new
+                {
+                    result.IsSuccess,
+                    result.Errors
+                });
+            }
+
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+            var lifeTimeRefresh = _config["Tokens:Refresh:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
+
+            Response.Cookies.Append(
+               "refreshToken",
+               result.Value.RefreshToken,
+               new CookieOptions
+               {
+                   HttpOnly = true,
+                   Secure = true,
+                   SameSite = SameSiteMode.None,
+                   Expires = DateTime.UtcNow.AddDays(int.TryParse(lifeTimeRefresh, out var days) ? days : 7),
+               });
 
             return Ok(new { result.IsSuccess, result.Value });
         }
@@ -119,6 +252,19 @@ namespace GlovoAPI.Controllers
             var result = await _mediator.Send(new VerifyResetCodeCommand(model));
 
             if (!result.IsSuccess) return BadRequest(new { result.IsSuccess, result.Errors });
+
+            var lifeTime = _config["Tokens:Jwt:LifeTime"];
+
+            Response.Cookies.Append(
+                "accessToken",
+                result.Value.AccessToken,
+                new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.None,
+                    Expires = DateTime.UtcNow.AddMinutes(int.TryParse(lifeTime, out var minutes) ? minutes : 15),
+                });
 
             return Ok(new { result.IsSuccess, result.Value });
         }
