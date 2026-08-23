91e345b61f644161a39bef8b1085139a039bd73e
MrSamix <alekssen2008@gmail.com>
2026-08-23 14:51:03 +0300

    add getPartnerProfile

---
+++ Core/Dtos/Partner/GetPartnerProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Dtos.Partner;
+
+public class GetPartnerProfileDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    // public string[] Roles { get; set; }
+}
