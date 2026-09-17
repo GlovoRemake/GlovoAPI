0b8d9a02560ca7ddaa94f25c10e3396e54a7628b
BloodyRoom <sabadashkamaksym@gmail.com>
2026-09-18 00:52:00 +0300

    add crd user addreses

---
+++ Core/Commands/Account/Address/AddAddressCommand.cs
@@ -0,0 +1,12 @@
+﻿using Core.Dtos;
+using Core.Dtos.Account;
+using Core.Dtos.Account.Address;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Commands.Account.Address;
+
+public record AddAddressCommand(Guid userId, AddAddressDto dto)
+    : IRequest<Result>;
---
+++ Core/Commands/Account/Address/DeleteAddressCommand.cs
@@ -0,0 +1,9 @@
+﻿using Core.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Commands.Account.Address;
+public record DeleteAddressCommand(Guid userId, int id)
+    : IRequest<Result>;
