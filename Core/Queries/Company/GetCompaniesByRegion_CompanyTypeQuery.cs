ea906822ccfeaa17e7eb1b94674e8bd857ac0c6a
Oleksandr Panchuk <alekssen2008@gmail.com>
2026-09-19 16:41:04 +0300

    add getCompanyByRegion and companyType(optional param)

---
+++ Core/Queries/Company/GetCompaniesByRegion_CompanyTypeQuery.cs
@@ -0,0 +1,8 @@
+﻿using Core.Dtos;
+using Core.Dtos.Company;
+using MediatR;
+
+namespace Core.Queries.Company;
+
+public record GetCompaniesByRegion_CompanyTypeQuery(int RegionId, int[]? CompanyTypeIds)
+    : IRequest<Result<List<CompanyDto?>>> { }
